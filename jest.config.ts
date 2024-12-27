7d6520ca7fd88bebddc1f76c8b5dc5392bd9c9df
fernando-pacheco <fernandopachecopx@gmail.com>
2024-12-26 23:18:08 -0300

    chore: configuração de range de cobertura removido

--- jest.config.ts
+++ jest.config.ts
@@ -7,12 +7,4 @@ module.exports = {
         },
     },
     collectCoverage: true,
-    coverageThreshold: {
-        global: {
-            branches: 50,
-            functions: 50,
-            lines: 50,
-            statements: 50,
-        },
-    },
 }
