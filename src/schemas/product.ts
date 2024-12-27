0087e7bd8447fc9b448c5025de55eba1752c877b
Fernando Pacheco <fernando.silva@sao.jamef.com.br>
2024-12-27 17:22:00 -0300

    refactor: uso de classes para gestão do serviço de Schemas

--- src/schemas/product.ts
+++ src/schemas/product.ts
@@ -1,23 +1,25 @@
 import { z } from "zod"
 
-export const ProductResponseSchema = z.object({
-    id: z.string(),
-    name: z.string(),
-    price: z.number(),
-})
+export class ProductSchemas {
+    public readonly response = z.object({
+        id: z.string(),
+        name: z.string(),
+        price: z.number(),
+    })
 
-export const ProductsListResponseSchema = z.array(ProductResponseSchema)
+    public readonly listResponse = z.array(this.response)
 
-export const ProductCreateSchema = z.object({
-    name: z.string().min(1, "Name is required"),
-    price: z.number().min(0, "Price is required"),
-})
+    public readonly create = z.object({
+        name: z.string().min(1, "Name is required"),
+        price: z.number().min(0, "Price is required"),
+    })
 
-export const ProductUpdateSchema = z.object({
-    name: z.string().optional(),
-    price: z.number().optional(),
-})
+    public readonly update = z.object({
+        name: z.string().optional(),
+        price: z.number().min(0).optional(),
+    })
 
-export const ProductIDParamsSchema = z.object({
-    id: z.string().min(1, "Product ID is required"),
-})
+    public readonly idParams = z.object({
+        id: z.string().min(1, "Product ID is required"),
+    })
+}
