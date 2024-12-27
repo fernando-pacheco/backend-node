ea380e465dde4c5e80313921f9afebb07de4ba18
Fernando Pacheco <fernando.silva@sao.jamef.com.br>
2024-12-27 17:18:03 -0300

    feat: classes abstratas para Routes, Resources e DocsSchemas

---
+++ src/interfaces/docs-schemas.ts
@@ -0,0 +1,13 @@
+import { FastifySchema } from "fastify"
+
+export abstract class DocsSchemas {
+    public abstract get create(): { schema: FastifySchema }
+
+    public abstract get list(): { schema: FastifySchema }
+
+    public abstract get get(): { schema: FastifySchema }
+
+    public abstract get update(): { schema: FastifySchema }
+
+    public abstract get delete(): { schema: FastifySchema }
+}
