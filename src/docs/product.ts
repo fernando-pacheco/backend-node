e6d7d3022e4ab12621f5e7fe47c261c8aaf29cfd
Fernando Pacheco <fernando.silva@sao.jamef.com.br>
2024-12-27 17:22:13 -0300

    refactor: uso de classes para gestão do serviço de DocsSchemas

--- src/docs/product.ts
+++ src/docs/product.ts
@@ -1,65 +1,78 @@
-import {
-    ProductCreateSchema,
-    ProductIDParamsSchema,
-    ProductResponseSchema,
-    ProductsListResponseSchema,
-    ProductUpdateSchema,
-} from "../schemas/product"
+import { DocsSchemas } from "../interfaces/docs-schemas"
+import { ProductSchemas } from "../schemas/product"
 import { MessageResponses } from "../utils/message-responses"
 
-export const ProductDocsSchemas = {
-    create: {
-        schema: {
-            tags: ["Products"],
-            description: "Create a new product",
-            body: ProductCreateSchema,
-            response: {
-                200: ProductResponseSchema,
-                ...MessageResponses([500]),
+export class ProductDocsSchemas extends DocsSchemas {
+    constructor(private schema: ProductSchemas = new ProductSchemas()) {
+        super()
+    }
+
+    public get create() {
+        return {
+            schema: {
+                tags: ["Products"],
+                description: "Create a new product",
+                body: this.schema.create,
+                response: {
+                    201: this.schema.response,
+                    ...MessageResponses([400, 500]),
+                },
             },
-        },
-    },
-    list: {
-        schema: {
-            tags: ["Products"],
-            description: "List products",
-            responses: {
-                200: ProductsListResponseSchema,
-                ...MessageResponses([500]),
+        }
+    }
+
+    public get list() {
+        return {
+            schema: {
+                tags: ["Products"],
+                description: "List products",
+                responses: {
+                    200: this.schema.listResponse,
+                    ...MessageResponses([500]),
+                },
             },
-        },
-    },
-    get: {
-        schema: {
-            tags: ["Products"],
-            description: "Get product by ID",
-            params: ProductIDParamsSchema,
-            response: {
-                200: ProductResponseSchema,
-                ...MessageResponses([404, 500]),
+        }
+    }
+
+    public get get() {
+        return {
+            schema: {
+                tags: ["Products"],
+                description: "Get product by ID",
+                params: this.schema.idParams,
+                response: {
+                    200: this.schema.response,
+                    ...MessageResponses([400, 404, 500]),
+                },
             },
-        },
-    },
-    update: {
-        schema: {
-            tags: ["Products"],
-            description: "Update product by ID",
-            body: ProductUpdateSchema,
-            params: ProductIDParamsSchema,
-            response: {
-                200: ProductResponseSchema,
-                ...MessageResponses([400, 404, 500]),
+        }
+    }
+
+    public get update() {
+        return {
+            schema: {
+                tags: ["Products"],
+                description: "Update product by ID",
+                body: this.schema.update,
+                params: this.schema.idParams,
+                response: {
+                    200: this.schema.response,
+                    ...MessageResponses([400, 404, 500]),
+                },
             },
-        },
-    },
-    delete: {
-        schema: {
-            tags: ["Products"],
-            description: "Delete product by ID",
-            params: ProductIDParamsSchema,
-            response: {
-                ...MessageResponses([200, 404, 500]),
+        }
+    }
+
+    public get delete() {
+        return {
+            schema: {
+                tags: ["Products"],
+                description: "Delete product by ID",
+                params: this.schema.idParams,
+                response: {
+                    ...MessageResponses([200, 400, 404, 500]),
+                },
             },
-        },
-    },
+        }
+    }
 }
