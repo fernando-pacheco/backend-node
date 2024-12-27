745422e243ce3fcfbc71ec9a5292dc9604579289
fernando-pacheco <fernandopachecopx@gmail.com>
2024-12-27 19:38:46 -0300

    feat: tipagem explícita

--- src/routes/product.ts
+++ src/routes/product.ts
@@ -1,3 +1,4 @@
+import { RouteHandlerMethod } from "fastify"
 import { ProductDocsSchemas } from "../docs/product"
 import { Routes } from "../interfaces/routes"
 import { ProductResources } from "../resources/product"
@@ -23,7 +24,7 @@ export class ProductRoutes extends Routes {
             endpoint: "/product",
             method: "post",
             docs: this.docsSchema.create,
-            resource: this.resources.create,
+            resource: this.resources.create as RouteHandlerMethod,
         })
 
         FabricRoute({
@@ -41,7 +42,7 @@ export class ProductRoutes extends Routes {
             endpoint: "/product/:id",
             method: "get",
             docs: this.docsSchema.get,
-            resource: this.resources.get,
+            resource: this.resources.get as RouteHandlerMethod,
         })
 
         FabricRoute({
@@ -49,7 +50,7 @@ export class ProductRoutes extends Routes {
             endpoint: "/product/:id",
             method: "put",
             docs: this.docsSchema.update,
-            resource: this.resources.update,
+            resource: this.resources.update as RouteHandlerMethod,
         })
 
         FabricRoute({
@@ -57,7 +58,7 @@ export class ProductRoutes extends Routes {
             endpoint: "/product/:id",
             method: "delete",
             docs: this.docsSchema.delete,
-            resource: this.resources.delete,
+            resource: this.resources.delete as RouteHandlerMethod,
         })
     }
 }
