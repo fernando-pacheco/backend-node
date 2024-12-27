377ba17de1d0e066b990e0357a2ac84d76c3ea97
Fernando Pacheco <fernando.silva@sao.jamef.com.br>
2024-12-27 12:34:26 -0300

    feat: registro das rotas de produto

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,7 @@
 import { FastifyInstance } from "fastify"
 import { UserHandlerRoutes, UserRegistersRoutes } from "./routes/user"
 import { PaymentHandlerRoutes, PaymentRegistersRoutes } from "./routes/payment"
+import { ProductHandlerRoutes, ProductRegistersRoutes } from "./routes/product"
 
 export async function routes(app: FastifyInstance) {
     // Users
@@ -10,4 +11,8 @@ export async function routes(app: FastifyInstance) {
     // Payments
     PaymentRegistersRoutes(app)
     PaymentHandlerRoutes(app)
+
+    // Products
+    ProductRegistersRoutes(app)
+    ProductHandlerRoutes(app)
 }
