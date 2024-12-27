2e9f29347b985862e1716082e9b45f7df9ec4b6b
fernando-pacheco <fernandopachecopx@gmail.com>
2024-12-27 20:20:48 -0300

    remove: métodos não utilizados

--- src/interfaces/docs-schemas.ts
+++ src/interfaces/docs-schemas.ts
@@ -1,9 +1,6 @@
 import { FastifySchema } from "fastify"
 
 export abstract class DocsSchemas {
-    public abstract create: { schema: FastifySchema }
-    public abstract get: { schema: FastifySchema }
-    public abstract list?: { schema: FastifySchema }
-    public abstract update?: { schema: FastifySchema }
-    public abstract delete?: { schema: FastifySchema }
+    public abstract get create(): { schema: FastifySchema }
+    public abstract get get(): { schema: FastifySchema }
 }
