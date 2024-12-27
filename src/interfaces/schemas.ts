5fd9b0f56983df17edc7639f2f5ae15fdde72889
Fernando Pacheco <fernando.silva@sao.jamef.com.br>
2024-12-27 17:40:17 -0300

    remove: classe abstrata não requerida

--- src/interfaces/schemas.ts
+++ src/interfaces/schemas.ts
@@ -1,6 +1,5 @@
 export abstract class Schemas {
     abstract get response(): any
-    abstract get listResponse(): any
     abstract get create(): any
     abstract get update(): any
     abstract get idParams(): any
