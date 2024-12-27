78c6ef97aa7fd0be65822886eb1d0efd991cb9a6
fernando-pacheco <fernandopachecopx@gmail.com>
2024-12-26 22:32:30 -0300

    feat: registro das rotas de pagamento

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,13 @@
 import { FastifyInstance } from "fastify"
 import { UserHandlerRoutes, UserRegistersRoutes } from "./routes/user"
+import { PaymentHandlerRoutes, PaymentRegistersRoutes } from "./routes/payment"
 
 export async function routes(app: FastifyInstance) {
+    // Users
     UserRegistersRoutes(app)
     UserHandlerRoutes(app)
+
+    // Payments
+    PaymentRegistersRoutes(app)
+    PaymentHandlerRoutes(app)
 }
