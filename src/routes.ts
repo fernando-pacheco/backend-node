d4c9cb10674eb7e021e0d00f63073ce8aafa3692
fernando-pacheco <fernandopachecopx@gmail.com>
2024-12-27 22:12:50 -0300

    feat: registro das rotas de usuário e pagamento

--- src/routes.ts
+++ src/routes.ts
@@ -1,12 +1,22 @@
+import { PaymentRoutes } from "./routes/payment"
 import { ProductRoutes } from "./routes/product"
+import { UserRoutes } from "./routes/user"
 import { FastifyTypedInstance } from "./types"
 
 export class Routes {
-    constructor(private product: ProductRoutes = new ProductRoutes()) {
+    constructor(
+        private user: UserRoutes = new UserRoutes(),
+        private product: ProductRoutes = new ProductRoutes(),
+        private payment: PaymentRoutes = new PaymentRoutes()
+    ) {
+        this.user = user
         this.product = product
+        this.payment = payment
     }
 
     register = async (app: FastifyTypedInstance) => {
+        this.user.registerRoutes(app)
         this.product.registerRoutes(app)
+        this.payment.registerRoutes(app)
     }
 }
