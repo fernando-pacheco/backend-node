c083db2acef32c9ec644761b7d8e2bc06fe4bfb0
fernando-pacheco <fernandopachecopx@gmail.com>
2024-12-28 01:43:48 -0300

    feat: registro das rotas item cart e cart

--- src/routes.ts
+++ src/routes.ts
@@ -1,3 +1,5 @@
+import { CartRoutes } from "./routes/cart"
+import { ItemCartRoutes } from "./routes/item-cart"
 import { PaymentRoutes } from "./routes/payment"
 import { ProductRoutes } from "./routes/product"
 import { UserRoutes } from "./routes/user"
@@ -7,16 +9,22 @@ export class Routes {
     constructor(
         private user: UserRoutes = new UserRoutes(),
         private product: ProductRoutes = new ProductRoutes(),
-        private payment: PaymentRoutes = new PaymentRoutes()
+        private payment: PaymentRoutes = new PaymentRoutes(),
+        private cart: CartRoutes = new CartRoutes(),
+        private itemCart: ItemCartRoutes = new ItemCartRoutes()
     ) {
         this.user = user
         this.product = product
         this.payment = payment
+        this.cart = cart
+        this.itemCart = itemCart
     }
 
     register = async (app: FastifyTypedInstance) => {
         this.user.registerRoutes(app)
         this.product.registerRoutes(app)
         this.payment.registerRoutes(app)
+        this.cart.registerRoutes(app)
+        this.itemCart.registerRoutes(app)
     }
 }
