18d7259aa1eda873eb1f3a78d68360064e47c761
fernando-pacheco <fernandopachecopx@gmail.com>
2024-12-27 22:10:43 -0300

    refactor: uso de classes para gestão das resources

--- src/resources/payment.ts
+++ src/resources/payment.ts
@@ -1,27 +1,51 @@
-import { createPayment, getPaymentByID } from "../service/payment"
-import { PaymentResponseSchema } from "../schemas/payment"
+import { PaymentServices } from "../service/payment"
+import { PaymentSchemas } from "../schemas/payment"
+import { Payment } from "@prisma/client"
+import { FastifyReply, FastifyRequest } from "fastify"
 
-export const PaymentResources = {
-    create: PaymentCreateResource,
-    get: PaymentGetResource,
-}
+export class PaymentResources {
+    constructor(
+        private service: PaymentServices = new PaymentServices(),
+        private schema: PaymentSchemas = new PaymentSchemas()
+    ) {
+        this.service = service
+        this.schema = schema
+    }
 
-async function PaymentCreateResource(request: any, reply: any) {
-    const body = request.body
-    const newPayment = await createPayment(body)
-    reply.status(201).send(newPayment)
-}
+    create = async (
+        request: FastifyRequest<{ Body: Payment }>,
+        reply: FastifyReply
+    ) => {
+        const body = request.body
+        const newPayment = await this.service.createPayment(body)
+        reply.status(201).send(newPayment)
+    }
 
-async function PaymentGetResource(request: any, reply: any) {
-    const { id } = request.params
-    try {
-        const payment = await getPaymentByID(id)
-        if (!payment) {
-            reply.status(404).send({ message: "Payment not found." })
-            return
+    get = async (
+        request: FastifyRequest<{ Params: Payment }>,
+        reply: FastifyReply
+    ) => {
+        const { id } = request.params
+        try {
+            const payment = await this.ensurePaymentExists(id, reply)
+            reply.status(200).send(this.schema.response.parse(payment))
+        } catch (error) {
+            this.handleError(reply, error, 400)
         }
-        reply.status(200).send(PaymentResponseSchema.parse(payment))
-    } catch (error) {
-        reply.status(500).send({ message: "Internal server error." })
+    }
+
+    private async ensurePaymentExists(id: string, reply: FastifyReply) {
+        const product = await this.service.getPaymentByID(id)
+        if (!product) {
+            reply.status(404).send({ message: "Payment not found." })
+            return null
+        }
+        return product
+    }
+
+    private handleError(reply: FastifyReply, error: unknown, statusCode = 500) {
+        const message =
+            error instanceof Error ? error.message : "Internal server error."
+        reply.status(statusCode).send({ message })
     }
 }
--- src/resources/user.ts
+++ src/resources/user.ts
@@ -1,83 +1,92 @@
-import {
-    createUser,
-    deleteUserByID,
-    getUserByID,
-    getUsers,
-    updateUser,
-} from "../service/user"
-import { UserResponseSchema } from "../schemas/user"
+import { UserServices } from "../service/user"
+import { UserSchemas } from "../schemas/user"
+import { Resources } from "../interfaces/resources"
+import { User } from "@prisma/client"
+import { FastifyReply, FastifyRequest } from "fastify"
 
-export const UserResources = {
-    create: UserCreateResource,
-    list: UsersListResource,
-    get: UserGetResource,
-    update: UserPutResource,
-    delete: UserDeleteResource,
-}
-
-async function UserCreateResource(request: any, reply: any) {
-    const body = request.body
-    const newUser = await createUser(body)
-    reply.status(201).send(newUser)
-}
-
-async function UsersListResource() {
-    const users = await getUsers()
-    return users
-}
-
-async function UserGetResource(request: any, reply: any) {
-    const { id } = request.params
-    try {
-        const user = await getUserByID(id)
-        if (!user) {
-            reply.status(404).send({ message: "User not found." })
-            return
-        }
-        reply.status(200).send(UserResponseSchema.parse(user))
-    } catch (error) {
-        reply.status(500).send({ message: "Internal server error." })
+export class UserResources extends Resources<User> {
+    constructor(
+        private service: UserServices = new UserServices(),
+        private schema: UserSchemas = new UserSchemas()
+    ) {
+        super()
     }
-}
 
-async function UserPutResource(request: any, reply: any) {
-    const { id } = request.params
-    const body = request.body
-    try {
-        const user = await getUserByID(id)
-
-        if (!user) {
-            reply.status(404).send({ message: "User not found." })
-            return
-        }
-
-        if (Object.keys(body).length === 0) {
-            reply.status(400).send({ message: "Empty body." })
-            return
-        }
-
-        const updatedUser = await updateUser(id, body)
-        reply.status(200).send(updatedUser)
-    } catch (error) {
-        reply.status(500).send({ message: "Internal server error." })
+    create = async (
+        request: FastifyRequest<{ Body: User }>,
+        reply: FastifyReply
+    ) => {
+        const body = request.body
+        const newUser = await this.service.createUser(body)
+        reply.status(201).send(newUser)
     }
-}
 
-async function UserDeleteResource(request: any, reply: any) {
-    const { id } = request.params
-    try {
-        const user = await getUserByID(id)
+    list = async () => {
+        const users = await this.service.getUsers()
+        return users
+    }
+
+    get = async (
+        request: FastifyRequest<{ Params: User }>,
+        reply: FastifyReply
+    ) => {
+        const { id } = request.params
+        try {
+            const user = await this.ensureUserExists(id, reply)
+            reply.status(200).send(this.schema.response.parse(user))
+        } catch (error) {
+            this.handleError(reply, error, 400)
+        }
+    }
+
+    update = async (
+        request: FastifyRequest<{ Body: User; Params: User }>,
+        reply: FastifyReply
+    ) => {
+        const { id } = request.params
+        const body = request.body
+        try {
+            await this.ensureUserExists(id, reply)
+            if (Object.keys(body).length === 0) {
+                reply.status(400).send({ message: "Empty body." })
+                return
+            }
+
+            const updatedUser = await this.service.updateUser(id, body)
+            reply.status(200).send(updatedUser)
+        } catch (error) {
+            this.handleError(reply, error, 400)
+        }
+    }
+
+    delete = async (
+        request: FastifyRequest<{ Params: User }>,
+        reply: FastifyReply
+    ) => {
+        const { id } = request.params
+        try {
+            await this.ensureUserExists(id, reply)
+            await this.service.deleteUserByID(id)
+            reply
+                .status(200)
+                .send({ message: `User {${id}} successfully deleted.` })
+        } catch (error) {
+            this.handleError(reply, error, 400)
+        }
+    }
+
+    private async ensureUserExists(id: string, reply: FastifyReply) {
+        const user = await this.service.getUserByID(id)
         if (!user) {
             reply.status(404).send({ message: "User not found." })
-            return
+            return null
         }
+        return user
+    }
 
-        await deleteUserByID(id)
-
-        reply
-            .status(200)
-            .send({ message: `User {${id}} successfully deleted.` })
-    } catch (error) {
-        reply.status(500).send({ message: "Internal server error." })
+    private handleError(reply: FastifyReply, error: unknown, statusCode = 500) {
+        const message =
+            error instanceof Error ? error.message : "Internal server error."
+        reply.status(statusCode).send({ message })
     }
 }
