b7c25fdc89905b48a72f015226932b0e3aa95aaf
fernando-pacheco <fernandopachecopx@gmail.com>
2024-12-28 01:43:00 -0300

    feat: módulo de documentação de rota para item cart e cart

---
+++ src/docs/cart.ts
@@ -0,0 +1,62 @@
+import { DocsSchemas } from "../interfaces/docs-schemas"
+import { CartSchemas } from "../schemas/cart"
+import { MessageResponses } from "../utils/message-responses"
+
+export class CartDocsSchemas {
+    constructor(private schema: CartSchemas = new CartSchemas()) {
+        this.schema = schema
+    }
+
+    public get create() {
+        return {
+            schema: {
+                tags: ["Carts"],
+                description: "Create a cart",
+                response: {
+                    201: this.schema.response,
+                    ...MessageResponses([400, 500]),
+                },
+            },
+        }
+    }
+
+    public get list() {
+        return {
+            schema: {
+                tags: ["Carts"],
+                description: "List carts",
+                responses: {
+                    200: this.schema.listResponse,
+                    ...MessageResponses([500]),
+                },
+            },
+        }
+    }
+
+    public get listItemsCart() {
+        return {
+            schema: {
+                tags: ["Carts"],
+                description: "List items by cart ID",
+                params: this.schema.idParams,
+                responses: {
+                    200: this.schema.listItemsCart,
+                    ...MessageResponses([500]),
+                },
+            },
+        }
+    }
+
+    public get get() {
+        return {
+            schema: {
+                tags: ["Carts"],
+                description: "Get cart by ID",
+                response: {
+                    201: this.schema.response,
+                    ...MessageResponses([400, 404, 500]),
+                },
+            },
+        }
+    }
+}
---
+++ src/docs/item-cart.ts
@@ -0,0 +1,65 @@
+import { DocsSchemas } from "../interfaces/docs-schemas"
+import { ItemCartSchemas } from "../schemas/item-cart"
+import { MessageResponses } from "../utils/message-responses"
+
+export class ItemCartDocsSchemas extends DocsSchemas {
+    constructor(private schema: ItemCartSchemas = new ItemCartSchemas()) {
+        super()
+    }
+
+    public get create() {
+        return {
+            schema: {
+                tags: ["ItemsCart"],
+                description: "Create a new item cart",
+                body: this.schema.create,
+                response: {
+                    201: this.schema.response,
+                    ...MessageResponses([400, 500]),
+                },
+            },
+        }
+    }
+
+    public get get() {
+        return {
+            schema: {
+                tags: ["ItemsCart"],
+                description: "Get product by ID",
+                params: this.schema.idParams,
+                response: {
+                    200: this.schema.response,
+                    ...MessageResponses([400, 404, 500]),
+                },
+            },
+        }
+    }
+
+    public get update() {
+        return {
+            schema: {
+                tags: ["ItemsCart"],
+                description: "Update product by ID",
+                body: this.schema.update,
+                params: this.schema.idParams,
+                response: {
+                    200: this.schema.response,
+                    ...MessageResponses([400, 404, 500]),
+                },
+            },
+        }
+    }
+
+    public get delete() {
+        return {
+            schema: {
+                tags: ["ItemsCart"],
+                description: "Delete product by ID",
+                params: this.schema.idParams,
+                response: {
+                    ...MessageResponses([200, 400, 404, 500]),
+                },
+            },
+        }
+    }
+}
