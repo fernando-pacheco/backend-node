01f0595cb439a0cbb5a95edfd7407cb1f14964d5
Fernando Pacheco <fernando.silva@sao.jamef.com.br>
2025-01-02 17:37:08 -0300

    chore: jest script e configurações

--- jest.config.ts
+++ jest.config.ts
@@ -1,10 +1,15 @@
 module.exports = {
     preset: "ts-jest",
     testEnvironment: "node",
-    globals: {
-        "ts-jest": {
-            isolatedModules: true,
-        },
+    transform: {
+        "^.+\\.ts$": ["ts-jest", { isolatedModules: true }],
     },
     collectCoverage: true,
+    coverageThreshold: {
+        global: {
+            functions: 80,
+            lines: 80,
+            statements: 80,
+        },
+    },
 }
